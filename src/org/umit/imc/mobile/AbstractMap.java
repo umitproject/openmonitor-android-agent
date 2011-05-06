3d873c1c94d0bae3f42a30a4ff973d168a287175
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-05-06 10:59:48 -0700

    Class AbstractMap added.

---
+++ src/org/umit/imc/mobile/AbstractMap.java
@@ -0,0 +1,23 @@
+package org.umit.imc.mobile;
+
+public class AbstractMap {
+		
+	
+	
+	public AbstractMap() {
+		super();
+		
+	}
+	
+	public boolean getEvents(){
+		return true;
+	}
+	
+	public boolean collateEvents(){
+		return true;
+	}
+	
+	public boolean setEvents(){
+		return true;
+	}
+}
