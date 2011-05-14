13089d23103c32b362895be696bad03790feabbf
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-05-14 07:08:46 -0700

    mock functions to test protobufs added to PBReadWrite

--- src/org/umit/icm/mobile/p2p/PBReadWrite.java
+++ src/org/umit/icm/mobile/p2p/PBReadWrite.java
@@ -18,3 +18,68 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  */
+
+package org.umit.icm.mobile.p2p;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.umit.icm.mobile.proto.generated.MessageProtos.*;
+
+public class PBReadWrite {
+	
+	private WebsiteReportNormal websiteNormal = WebsiteReportNormal.newBuilder()
+	.setBandwidth(128.9)
+	.setMatchPattern(true)
+	.setResponseContent("Content")
+	.setStatusCode(1)
+	.setTestID(10)
+	.setTimeUTC(123456)
+	.build();
+	
+
+	public void testWrite(){
+		FileOutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream("/tmp/temp.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			websiteNormal.writeTo(outputStream);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			outputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public WebsiteReportNormal testRead(){
+		FileInputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream("/tmp/temp.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebsiteReportNormal.newBuilder().build();
+		try {
+			WebsiteReportNormal newWebsiteNormal = WebsiteReportNormal.parseFrom(inputStream);
+			return newWebsiteNormal;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+}
+
