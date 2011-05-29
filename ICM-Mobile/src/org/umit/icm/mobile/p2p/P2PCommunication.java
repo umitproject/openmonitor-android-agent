4a8924c4592a14801dd884711cfffe62c96311f0
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-05-29 03:06:36 -0700

    sendMessagePublic and checkResponse methods added to P2P Communication.java

--- ICM-Mobile/src/org/umit/icm/mobile/p2p/P2PCommunication.java
+++ ICM-Mobile/src/org/umit/icm/mobile/p2p/P2PCommunication.java
@@ -21,13 +21,41 @@
 
 package org.umit.icm.mobile.p2p;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 import org.umit.icm.mobile.proto.MessageProtos.AgentData;
+import org.umit.icm.mobile.proto.MessageProtos.ResponseHeader;
 import org.umit.icm.mobile.utils.AESCrypto;
+import org.umit.icm.mobile.utils.RSACrypto;
 
 public class P2PCommunication {
 	public static byte[] sendMessage(AgentData agentInfo, byte[] message) throws Exception {
-		byte [] symmetricKey = AESCrypto.readKey("myPrivateKey.priv");
+		byte [] symmetricKey = AESCrypto.readKey("mySecretKey.priv");
 		byte [] cipherBytes = AESCrypto.encrypt(symmetricKey, message);
+		// TODO HTTP send
+		// TODO HTTP respond
 		return AESCrypto.decrypt(symmetricKey, cipherBytes);
+	}
+	
+	public static byte[] sendMessagePublic(AgentData agentInfo, byte[] message) throws Exception {
+		PrivateKey privateKey = RSACrypto.readPrivateKey("myPrivateKey.priv");
+		byte [] cipherBytes = RSACrypto.encryptPrivate(privateKey, message);
+		// TODO HTTP send
+		// TODO HTTP respond
+		ResponseHeader header = null;
+		checkResponse(header);
+		PublicKey peerPublicKey = RSACrypto.readPublicKey(agentInfo.getAgentIP()+"PublicKey.pub");
+		return RSACrypto.decryptPublic(peerPublicKey, cipherBytes);
+	}
+	
+	public static void checkResponse(ResponseHeader header) throws Exception {
+		int myCurrentTestVersionNo = 0, myCurrentVersionNo = 0;
+		if (header.getCurrentTestVersionNo() > myCurrentTestVersionNo)
+			;
+			// contact aggregator to update testversion
+		if (header.getCurrentVersionNo() > myCurrentVersionNo)
+			;
+			// contact aggregator to update agentversion
 	}
 }
