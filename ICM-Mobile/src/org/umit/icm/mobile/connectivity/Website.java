04acce73680572ff3bf91a102625cf91fb94d30c
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-05-31 12:42:14 -0700

    Class Website.java added to hold the result of a website test.

---
+++ ICM-Mobile/src/org/umit/icm/mobile/connectivity/Website.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2011 Adriano Monteiro Marques
+ *
+ * Author:  Zubair Nabi <zn.zubairnabi@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+package org.umit.icm.mobile.connectivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Website {
+	private String url;
+	private String content;
+	private List<String> header;
+	
+	public Website(String url, String content, List<String> header) {
+		super();
+		this.url = url;
+		this.content = content;
+		this.header = header;
+	}
+	
+	public Website() {
+		super();
+		this.url = new String();
+		this.content = new String();
+		this.header = new ArrayList<String>();
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<String> getHeader() {
+		return header;
+	}
+
+	public void setHeader(List<String> header) {
+		this.header = header;
+	}
+	
+}
