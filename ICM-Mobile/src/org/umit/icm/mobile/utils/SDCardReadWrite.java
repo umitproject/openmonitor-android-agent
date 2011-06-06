7efd3cfd3a1b51f29bcb7c83e997541080a4e787
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-06-06 13:28:11 -0700

    website report read/write path changed by replacing / in url with -

--- ICM-Mobile/src/org/umit/icm/mobile/utils/SDCardReadWrite.java
+++ ICM-Mobile/src/org/umit/icm/mobile/utils/SDCardReadWrite.java
@@ -160,7 +160,8 @@ public class SDCardReadWrite {
 		File keyDir = new File (sdCard.getAbsolutePath() 
     			+ dir);
 		keyDir.mkdirs();
-    	File file = new File(keyDir, data.getReport().getWebsiteURL().substring(10)
+    	File file = new File(keyDir
+    			, data.getReport().getWebsiteURL().substring(11).replaceAll("/", "-")
     			+ Constants.WEBSITE_FILE);
     	if(!file.exists()){
     		file.createNewFile();
@@ -179,7 +180,8 @@ public class SDCardReadWrite {
 		sdCard = Environment.getExternalStorageDirectory();
 		File keyDir = new File (sdCard.getAbsolutePath() 
     			+ dir);
-    	File file = new File(keyDir, url.substring(10)
+    	File file = new File(keyDir
+    			, url.substring(11).replaceAll("/", "-")
     			+ Constants.WEBSITE_FILE);
     	InputStream inputStream = new FileInputStream(file.toString());
   	  	
