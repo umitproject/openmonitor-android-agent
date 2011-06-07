4e159b3b56d3d4759fb6d404fc45ba99d0501991
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-06-07 10:34:43 -0700

    More methods added to AggregatorResources.java

--- ICM-Mobile/src/org/umit/icm/mobile/aggregator/AggregatorResources.java
+++ ICM-Mobile/src/org/umit/icm/mobile/aggregator/AggregatorResources.java
@@ -21,13 +21,136 @@
 
 package org.umit.icm.mobile.aggregator;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
+import org.umit.icm.mobile.proto.MessageProtos.GetEvents;
+import org.umit.icm.mobile.proto.MessageProtos.GetEventsResponse;
+import org.umit.icm.mobile.proto.MessageProtos.GetPeerList;
+import org.umit.icm.mobile.proto.MessageProtos.GetPeerListResponse;
+import org.umit.icm.mobile.proto.MessageProtos.GetSuperPeerList;
+import org.umit.icm.mobile.proto.MessageProtos.GetSuperPeerListResponse;
+import org.umit.icm.mobile.proto.MessageProtos.NewTests;
+import org.umit.icm.mobile.proto.MessageProtos.NewTestsResponse;
+import org.umit.icm.mobile.proto.MessageProtos.NewVersion;
+import org.umit.icm.mobile.proto.MessageProtos.NewVersionResponse;
+import org.umit.icm.mobile.proto.MessageProtos.RegisterAgent;
+import org.umit.icm.mobile.proto.MessageProtos.RegisterAgentResponse;
+import org.umit.icm.mobile.proto.MessageProtos.SendReportResponse;
+import org.umit.icm.mobile.proto.MessageProtos.SendServiceReport;
+import org.umit.icm.mobile.proto.MessageProtos.SendWebsiteReport;
+import org.umit.icm.mobile.proto.MessageProtos.ServiceSuggestion;
+import org.umit.icm.mobile.proto.MessageProtos.TestSuggestionResponse;
+import org.umit.icm.mobile.proto.MessageProtos.WebsiteSuggestion;
 import org.umit.icm.mobile.utils.Constants;
 
 public class AggregatorResources {
 
 	 public ClientResource getClientResource(String url) {
 		 return new ClientResource(Constants.AGGREGATOR_URL + url);
+	 }
+	 
+	 public RegisterAgentResponse registerAgent(
+			 RegisterAgent registerAgent,
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(registerAgent); 
+		 return RegisterAgentResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public GetPeerListResponse getPeerList(
+			 GetPeerList getPeerList, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(getPeerList); 
+		 return GetPeerListResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public GetSuperPeerListResponse getSuperPeerList(
+			 GetSuperPeerList getSuperPeerList, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(getSuperPeerList); 
+		 return GetSuperPeerListResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public GetEventsResponse getEvents(
+			 GetEvents getEvents, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(getEvents); 
+		 return GetEventsResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public SendReportResponse sendWebsiteReport(
+			 SendWebsiteReport sendWebsiteReport, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(sendWebsiteReport); 
+		 return SendReportResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public SendReportResponse sendServiceReport(
+			 SendServiceReport sendServiceReport, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(sendServiceReport); 
+		 return SendReportResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public NewVersionResponse checkVersion(
+			 NewVersion newVersion, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(newVersion); 
+		 return NewVersionResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public NewTestsResponse checkTests(
+			 NewTests newTests, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(newTests); 
+		 return NewTestsResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public TestSuggestionResponse sendWebsiteSuggestion(
+			 WebsiteSuggestion websiteSuggestion, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(websiteSuggestion); 
+		 return TestSuggestionResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
+	 }
+	 
+	 public TestSuggestionResponse sendServiceSuggestion(
+			 ServiceSuggestion serviceSuggestion, 
+			 ClientResource clientResource) 
+	 throws UnsupportedEncodingException, IOException {
+		 Representation response = clientResource.post(serviceSuggestion); 
+		 return TestSuggestionResponse.parseFrom(
+				 new ByteArrayInputStream(
+						 response.getText().getBytes("UTF-8")));
 	 }
 
 }
