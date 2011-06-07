407bbea8a1554ecde50eca387945b1b3e2041b98
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-06-07 12:14:45 -0700

    TestManager.java added with setters/getters added.

---
+++ ICM-Mobile/src/org/umit/icm/mobile/process/TestManager.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2011 Adriano Monteiro Marques
+ *
+ * Author:  Zubair Nabi <zn.zubairnabi@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either Test 2 of the License, or
+ * (at your option) any later Test.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+package org.umit.icm.mobile.process;
+
+public class TestManager {
+	private int currentTest;
+	
+	TestManager() {
+		currentTest = 0;
+	}
+
+	public int getCurrentTest() {
+		return currentTest;
+	}
+
+	public void setCurrentTest(int currentTest) {
+		this.currentTest = currentTest;
+	}
+}
