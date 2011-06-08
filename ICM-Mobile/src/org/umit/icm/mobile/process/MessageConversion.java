74ea4f4d1a91176b36de108de836896c225b4f57
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-06-08 14:15:55 -0700

    newTestsResponseAdd method added to add all new tests from the responseheader to the TestManager test list.

--- ICM-Mobile/src/org/umit/icm/mobile/process/MessageConversion.java
+++ ICM-Mobile/src/org/umit/icm/mobile/process/MessageConversion.java
@@ -23,17 +23,18 @@ package org.umit.icm.mobile.process;
 
 import java.io.IOException;
 
+import org.umit.icm.mobile.proto.MessageProtos.NewTestsResponse;
 import org.umit.icm.mobile.proto.MessageProtos.ResponseHeader;
 import org.umit.icm.mobile.proto.MessageProtos.Test;
 
 public class MessageConversion {
-	public static TestObject TestToTestObject (Test test) {
+	public static TestObject testToTestObject (Test test) {
 		return new TestObject(
 				test.getTestID(), test.getWebsiteURL(),
 				test.getServideCode(), test.getExecuteAtTimeUTC());
 	}
 	
-	public static void UpdateAgentVersion (ResponseHeader header) throws IOException {
+	public static void updateAgentVersion (ResponseHeader header) throws IOException {
 		VersionManager versionManager = new VersionManager();
 		if (header.getCurrentVersionNo() > versionManager.getAgentVersion()) {
 			versionManager.setAgentVersion(header.getCurrentVersionNo());
@@ -41,11 +42,26 @@ public class MessageConversion {
 		}
 	}
 	
-	public static void UpdateTestsVersion (ResponseHeader header) throws IOException {
+	public static void updateTestsVersion (ResponseHeader header) throws IOException {
 		VersionManager versionManager = new VersionManager();
 		if (header.getCurrentTestVersionNo() > versionManager.getTestsVersion()) {
 			versionManager.setTestsVersion(header.getCurrentTestVersionNo());
 			// TODO update current tests
+		}
+	}
+	
+	public static void newTestsResponseAdd (NewTestsResponse response) {
+		TestManager testManager = new TestManager();
+		for (int i = 0 ; i < response.getTestsCount() ; i++) {
+			try {
+				testManager.addTest(testToTestObject(response.getTests(i)));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
