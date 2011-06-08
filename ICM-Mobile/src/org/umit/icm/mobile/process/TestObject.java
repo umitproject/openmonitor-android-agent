6cdb757b19667b1da0108a8b0dcb434fa1849938
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-06-08 12:37:06 -0700

    TestObject.java added to hold test details.

---
+++ ICM-Mobile/src/org/umit/icm/mobile/process/TestObject.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) 2011 Adriano Monteiro Marques
+ *
+ * Author:  Zubair Nabi <zn.zubairnabi@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either Test 2 of the License, or
+ * (at your option) any later Test.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+package org.umit.icm.mobile.process;
+
+public class TestObject {
+	private long testID;
+	private String websiteURL;
+	private int serviceCode;
+	private long executeAtTimeUTC;
+	
+	public TestObject(long testID, String websiteURL, int serviceCode,
+			long executeAtTimeUTC) {
+		super();
+		this.testID = testID;
+		this.websiteURL = websiteURL;
+		this.serviceCode = serviceCode;
+		this.executeAtTimeUTC = executeAtTimeUTC;
+	}
+
+	public TestObject() {
+		testID = 0;
+		websiteURL = null;
+		serviceCode = 0;
+		executeAtTimeUTC = 0;
+		
+	}
+
+	public long getTestID() {
+		return testID;
+	}
+
+	public void setTestID(long testID) {
+		this.testID = testID;
+	}
+
+	public String getWebsiteURL() {
+		return websiteURL;
+	}
+
+	public void setWebsiteURL(String websiteURL) {
+		this.websiteURL = websiteURL;
+	}
+
+	public int getServiceCode() {
+		return serviceCode;
+	}
+
+	public void setServiceCode(int serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+
+	public long getExecuteAtTimeUTC() {
+		return executeAtTimeUTC;
+	}
+
+	public void setExecuteAtTimeUTC(long executeAtTimeUTC) {
+		this.executeAtTimeUTC = executeAtTimeUTC;
+	}
+}
