27f7fec7aebdb3029282b5749f248f6f57d16af0
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-07-08 12:13:29 -0700

    change after ip added as field to service.

--- icm-mobiletest/src/org/umit/icm/mobile/test/connectivity/ServiceReadWriteTests.java
+++ icm-mobiletest/src/org/umit/icm/mobile/test/connectivity/ServiceReadWriteTests.java
@@ -38,7 +38,7 @@ public class ServiceReadWriteTests extends AndroidTestCase {
     	List<Integer> ports = new ArrayList<Integer>();
     	ports.add(8080);
     	ports.add(8000);
-    	Service service = new Service("name", ports , "status", "check");
+    	Service service = new Service("name", ports, "ip", "status", "check");
     	service.writeService();
     	Service newService 
     	= service.readService(service.getName());
