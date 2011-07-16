120d950f7023c4478e28ef3bb6903e54be5c3bdc
Zubair Nabi <zn.zubairnabi@gmail.com>
2011-07-16 03:43:34 -0700

    setAccessToken method added.

--- icm-mobile/src/org/umit/icm/mobile/social/TwitterUpdate.java
+++ icm-mobile/src/org/umit/icm/mobile/social/TwitterUpdate.java
@@ -49,8 +49,8 @@ public class TwitterUpdate {
 		requestToken = null;
 	}
 	
-	public void setAccessToken() {
-		
+	public void setAccessToken(AccessToken accessToken) {
+		this.accessToken = accessToken;
 	}
 	
 	public void sendTweet(String tweet) throws TwitterException {
