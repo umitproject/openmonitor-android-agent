d83277cf5541674c2af7ef4843c0797b16ad1271
Luís A. Bastião Silva <luis.kop@gmail.com>
2012-02-17 03:39:56 -0800

    Added deps.sh - file to automatically deploy maven dependences (local ones)

---
+++ deps.sh
@@ -0,0 +1,37 @@
+wget http://dev.umitproject.org/attachments/download/345/osmdroid-3.0.4.jar
+wget http://dev.umitproject.org/attachments/download/346/maps-4_r1.jar
+wget http://javamail-android.googlecode.com/files/additionnal.jar
+wget http://javamail-android.googlecode.com/files/mail.jar
+wget http://javamail-android.googlecode.com/files/activation.jar
+mvn install:install-file  -Dfile=osmdroid-3.0.4.jar \
+                          -DgroupId=org.osmdroid \
+                          -DartifactId=osmdroid \
+                          -Dversion=3.0.4 \
+                          -Dpackaging=jar 
+
+
+mvn install:install-file  -Dfile=osmdroid-3.0.4.jar \
+                          -DgroupId=com.google.android.maps \
+                          -DartifactId=maps \
+                          -Dversion=4_r1 \
+                          -Dpackaging=jar 
+
+
+mvn install:install-file  -Dfile=mail.jar \
+                          -DgroupId=javamail-android \
+                          -DartifactId=mail \
+                          -Dversion=1.0 \
+                          -Dpackaging=jar 
+
+mvn install:install-file  -Dfile=activation.jar \
+                          -DgroupId=javamail-android \
+                          -DartifactId=activation \
+                          -Dversion=1.0\
+                          -Dpackaging=jar 
+
+mvn install:install-file  -Dfile=additionnal.jar\
+                          -DgroupId=javamail-android\
+                          -DartifactId=additional\
+                          -Dversion=1.0\
+                          -Dpackaging=jar 
+
