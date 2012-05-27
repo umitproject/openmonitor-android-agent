2b93a568da13292f743d0d08c515d7d27598d5a8
Gautam Bajaj <gautam1237@gmail.com>
2012-05-27 05:34:57 +0530

    Changed BugReport UI

---
+++ icm-mobile/src/org/umit/icm/mobile/gui/BugReportActivity.java
@@ -0,0 +1,15 @@
+package org.umit.icm.mobile.gui;
+
+import org.umit.icm.mobile.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BugReportActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bugreportactivity);
+	}
+
+}
