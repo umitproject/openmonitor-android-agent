4cbcc4342bfef551ce42654341e95bf7c9646978
Narendran Thangarajan <narendran.thangarajan@gmail.com>
2012-05-29 14:43:13 +0530

    Added the boost static libs

--- icm-mobile/jni/Android.mk
+++ icm-mobile/jni/Android.mk
@@ -6,6 +6,12 @@ include $(CLEAR_VARS)
 
 LOCAL_PATH := $(MY_PATH)
 
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/boost/include/
+LOCAL_LDLIBS += -L$(LOCAL_PATH)/boost/lib/ -lboost_date_time -lboost_program_options -lboost_regex
+
+LOCAL_CPPFLAGS += -fexceptions
+LOCAL_CPPFLAGS += -frtti
+
 LOCAL_LDLIBS := -llog -ldl
   
 LOCAL_MODULE    := boost-lib  
--- icm-mobile/obj/local/armeabi/objs/boost-lib/test_boost.o.d
+++ icm-mobile/obj/local/armeabi/objs/boost-lib/test_boost.o.d
@@ -1,7 +1,7 @@
 obj/local/armeabi/objs/boost-lib/test_boost.o: jni/test_boost.cpp \
  jni/org_umit_icm_mobile_Main.h \
- /home/sunshadow/Documents/android-ndk-r8/platforms/android-4/arch-arm/usr/include/jni.h
+ /home/sunshadow/Documents/android-ndk-r7-crystax-5.beta2/platforms/android-4/arch-arm/usr/include/jni.h
 
 jni/org_umit_icm_mobile_Main.h:
 
-/home/sunshadow/Documents/android-ndk-r8/platforms/android-4/arch-arm/usr/include/jni.h:
+/home/sunshadow/Documents/android-ndk-r7-crystax-5.beta2/platforms/android-4/arch-arm/usr/include/jni.h:
