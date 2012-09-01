3b40a3a573cc23078ea1ae4e12fe4afccee5c787
Zubair Nabi <zubair.nabi@cl.cam.ac.uk>
2012-09-01 16:32:42 +0500

    Benchmark and WebsiteThrottling cleaned up

--- icm-mobile/src/org/umit/icm/mobile/connectivity/Benchmark.java
+++ icm-mobile/src/org/umit/icm/mobile/connectivity/Benchmark.java
@@ -8,18 +8,12 @@ public class Benchmark {
 	
 	static Iterator<Website> websiteList = Globals.websitesList.iterator();
 	
+	public static double DeviationFromAverageThroughput(String website) {
+		
+		double throughput = WebsiteThrottling.Throughput(website);	
+		double averageThroughput = Globals.runtimeParameters.getAverageThroughput();
+		return Math.abs(averageThroughput - throughput);
 	
-	
-	public static double DeviationFromAverageThroughput(String website){
-		
-		double throughput=WebsiteThrottling.Throughput(website);
-		
-		double averageThroughput= Globals.runtimeParameters.getAverageThroughput();
-		
-		double deviation = Math.abs(averageThroughput-throughput);
-		
-		return deviation;
-		
 	}
 
 }
--- icm-mobile/src/org/umit/icm/mobile/connectivity/WebsiteThrottling.java
+++ icm-mobile/src/org/umit/icm/mobile/connectivity/WebsiteThrottling.java
@@ -5,39 +5,34 @@ import java.net.URLConnection;
 
 public class WebsiteThrottling {
 	
-	
-	public static long TimeTakenToDownload(String website){
-		long time_taken=0;
-		try{
+	public static long TimeTakenToDownload(String website) {
+		
+		long timeTaken = 0;
+		try {
 			URLConnection urlConnection = WebsiteOpen.openURLConnection(website);
-			long start_time=System.currentTimeMillis();
+			long startTime = System.currentTimeMillis();
 			WebsiteOpen.getContent(urlConnection);
-			long end_time=System.currentTimeMillis();
-			time_taken=end_time-start_time;
-		}catch(Exception e){
+			long endTime = System.currentTimeMillis();
+			timeTaken = endTime - startTime;
+		} catch(Exception e){
 			e.printStackTrace();
 		}
-		return time_taken;
+		return timeTaken;
 	}
 	
 	public static double Throughput(String website){
-		
-		double throughput=0;
-		try{
-		URLConnection urlConnection = WebsiteOpen.openURLConnection(website);
-		
-		String content=WebsiteOpen.getContent(urlConnection);
-		
-		long size= content.getBytes().length;
-		long time= WebsiteThrottling.TimeTakenToDownload(website);
-		
-		throughput=size/time;
-		}catch(Exception e){
-			
-		}
-		
-		return throughput;
-	}
 	
+		double throughput = 0;
+		try{
+			URLConnection urlConnection = WebsiteOpen.openURLConnection(website);
+			String content = WebsiteOpen.getContent(urlConnection);
+			long size = content.getBytes().length;
+			long time = WebsiteThrottling.TimeTakenToDownload(website);		
+			throughput = size/time;
+		}catch(Exception e){
+			e.printStackTrace();
+		}	
+		return throughput;
+	}	
 
 }
