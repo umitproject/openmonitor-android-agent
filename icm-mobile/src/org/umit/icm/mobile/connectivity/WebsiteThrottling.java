ddb2c3f0132aa06fe64b7f165b561b5a8a36a0f7
Zubair Nabi <zubair.nabi@cl.cam.ac.uk>
2012-09-01 15:30:35 +0500

    Throttling package removed and classes moved to connectivity.

---
+++ icm-mobile/src/org/umit/icm/mobile/connectivity/WebsiteThrottling.java
@@ -0,0 +1,43 @@
+package org.umit.icm.mobile.connectivity;
+
+import java.net.URLConnection;
+
+
+public class WebsiteThrottling {
+	
+	
+	public static long TimeTakenToDownload(String website){
+		long time_taken=0;
+		try{
+			URLConnection urlConnection = WebsiteOpen.openURLConnection(website);
+			long start_time=System.currentTimeMillis();
+			WebsiteOpen.getContent(urlConnection);
+			long end_time=System.currentTimeMillis();
+			time_taken=end_time-start_time;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return time_taken;
+	}
+	
+	public static double Throughput(String website){
+		
+		double throughput=0;
+		try{
+		URLConnection urlConnection = WebsiteOpen.openURLConnection(website);
+		
+		String content=WebsiteOpen.getContent(urlConnection);
+		
+		long size= content.getBytes().length;
+		long time= WebsiteThrottling.TimeTakenToDownload(website);
+		
+		throughput=size/time;
+		}catch(Exception e){
+			
+		}
+		
+		return throughput;
+	}
+	
+
+}
